428794ff92e1e4f1435f511ce0b8ae37e9670dba
Cookie Vang <vang2660@morris.umn.edu>
2020-08-31 16:25:24 -0500

    Add cat command to make tests pass

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -5,5 +5,7 @@ old=$1
 specifier=$2
 new=$3
 
-header="$specifier"+_header.html
-footer="$specifier"+_footer.html
+header="$specifier"_header.html
+footer="$specifier"_footer.html
+
+cat "$header" "$old" "$footer" > "$new"
