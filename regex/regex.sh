2db71049dc3c396090b42cdb70e97b80b715b3f6
Cookie Vang <vang2660@morris.umn.edu>
2020-08-31 22:33:38 -0500

    Add file 3 regex and all output files

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt
+sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
+
+#sed < r1_input.txt > r1_output.txt
+sed -E 's/\* ([a-zA-Z ]+) ([a-zA-Z\.]+) ([a-zA-Z ]+)/1. \2\n2. \3 \n /'  < r2_input.txt > r2_output.txt
